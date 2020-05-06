8407c0f7c607dc33874287df2f56066dfb3f2c4a
jdspell <jdspell@iu.edu>
2020-05-05 21:41:17 -0500

    added class definition of blockchain

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ class Block {
             JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "01/01/2020", "Genesis Block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
